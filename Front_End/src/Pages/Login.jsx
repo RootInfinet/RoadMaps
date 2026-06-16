8e4e749f867b3d32bee34616cb9de1168f5b6413
rootinfinet-eng <rootinfinet@gmail.com>
2026-06-16 07:18:27 +0300

    Adjusting Sittings

--- Front_End/src/Pages/Login.jsx
+++ Front_End/src/Pages/Login.jsx
@@ -1,11 +1,15 @@
-import React from 'react'
+import React from "react";
 
 function Login() {
   return (
     <div className="bg-[#0A0A0A] min-h-screen text-white font-sans">
-
+      <div className="flex flex-row justify-around items-center pt-5">
+        <h2 className="text-[#39FF14] text-4xl font-bold">RoodMaps</h2>
+        
+        <p className="text-[#BACCB0]">HELP_CENTER</p>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Login
+export default Login;
