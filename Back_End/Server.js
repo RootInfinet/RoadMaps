bb97b6e6a7d4d8f6517ecdf28b6b2f654c9809e0
rootinfinet-eng <rootinfinet@gmail.com>
2026-07-05 07:39:20 +0300

    update2

--- Back_End/Server.js
+++ Back_End/Server.js
@@ -6,16 +6,19 @@ const cors = require("cors");
 require("dotenv").config();
 
 const app = express();
-app.use(express.json({ limit: '10kb' }));
-app.use(cookieParser());
-app.use(helmet());
 
 app.use(
   cors({
-    origin: process.env.FRONT_URL || "http://localhost:5173",
+    origin: process.env.FRONT_URL || "http://localhost:5173", 
     credentials: true,
   })
 );
+
+app.use(express.json({ limit: '10kb' }));
+app.use(cookieParser());
+app.use(helmet({
+  crossOriginResourcePolicy: { policy: "cross-origin" } 
+}));
 
 app.use("/", authRoutes);
 
