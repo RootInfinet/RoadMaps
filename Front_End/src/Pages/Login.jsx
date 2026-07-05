ea528d7993565f8859b6029b8b052017c622ca46
rootinfinet-eng <rootinfinet@gmail.com>
2026-07-05 12:40:00 +0300

    final update

--- Front_End/src/Pages/Login.jsx
+++ Front_End/src/Pages/Login.jsx
@@ -26,7 +26,6 @@ function Login() {
       });
       if (response.status === 200) {
         alert("System Access Granted!");
-        localStorage.setItem("isLoggedIn", "true");
         dispatch(loginSuccess(response.data.user));
         navigate("/profile");
       }
